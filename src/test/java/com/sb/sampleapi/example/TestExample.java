11b88cb1439769e2f03a5eb06e8505d4fb15f506
subeenjeon <145312273+nasubeeni120@users.noreply.github.com>
2024-01-31 11:31:29 +0900

    Close #7

---
+++ src/test/java/com/sb/sampleapi/example/TestExample.java
@@ -0,0 +1,38 @@
+package com.sb.sampleapi.example;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestExample {
+
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+
+    }
+
+    @Test
+    void succedingTest() {
+    }
+
+    @Test
+    void failingTest() {
+        fail("테스트 실패");
+    }
+
+    @Test
+    public void addTest() {
+        assertEquals(210, Integer.sum(10, 200));
+    }
+
+
+}
